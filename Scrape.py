ca51d74e583924c457a853146695f816812b6bdf
Dominic Fate <dfate@ltisolutions.com>
2025-08-22 14:25:38 -0500

    added command line interface

--- Scrape.py
+++ Scrape.py
@@ -2,6 +2,7 @@
 #Desc: Code to scrape IP Unique IP addresses from Entra Sign-in logs and run them against the AbuseIPDB
 
 import AbuseAPI
+import sys
 
 IP_set = set()
 
@@ -19,18 +20,24 @@ def genIPrep(IP_set):
     file.close()
 
 
+def main():
+
+    try:
+        file_pointer = open(sys.argv[1], 'r+') #download recent stuff and change this var
+        filedata = file_pointer.readlines()
+        for line in filedata:
+            split_values = line.split('","')
+            if split_values[25][1].isdigit():
+                IP_set.add(split_values[25])
+
+        print(IP_set)
+        genIPrep(IP_set)
+    except FileNotFoundError:
+        print(f"Error: The file '{sys.argv[1]}' was not found.")
+    except IOError as e:
+        print(f"Error opening or reading file '{sys.argv[1]}': {e}")
+
 
 
 if __name__ == '__main__':
-    file_pointer = open('InteractiveSignIns_2025-08-21_2025-08-22.csv', 'r+') #download recent stuff and change this var
-    filedata = file_pointer.readlines()
-    for line in filedata:
-        split_values = line.split('","')
-        if split_values[25][1].isdigit():
-            IP_set.add(split_values[25])
-
-    print(IP_set)
-    genIPrep(IP_set)
-
-
-
+    main()
