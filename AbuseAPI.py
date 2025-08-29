4cb1056ca342d05090825884e8d1519723e51476
Dominic Fate <dfate@ltisolutions.com>
2025-08-29 15:06:33 -0500

    Updated the Scrape.py file to include logic to enable the storage of logins
    Updated the AbuseAPI.py to include the function to send API calls
     Please enter the commit message for your changes. Lines starting

--- AbuseAPI.py
+++ AbuseAPI.py
@@ -12,7 +12,7 @@ def API_request(ip):
 
     headers = {
         'Accept': 'application/json',
-        'Key': '' #define your own key
+        'Key': 'd67725b858a777216a5680f12446b24737533f828b3212e84be09c3b4cf9d5d55160a8b4a6d7de8c' #define your own key
     }
 
     response = requests.request(method='GET', url=url, headers=headers, params=querystring)
@@ -20,3 +20,16 @@ def API_request(ip):
     # Formatted output
     decodedResponse = json.loads(response.text)
     return json.dumps(decodedResponse, sort_keys=True, indent=4)
+
+def genIPrep(IP_set):
+    file = open('IP.txt', 'a')
+    for ip in IP_set:
+        response = API_request(ip)
+        response_row = response.split(',')
+
+        print(response_row[0])
+        print(response_row[4])
+
+        file.write(response)
+
+    file.close()
--- Scrape.py
+++ Scrape.py
@@ -2,36 +2,24 @@
 #Desc: Code to scrape IP Unique IP addresses from Entra Sign-in logs and run them against the AbuseIPDB
 
 import AbuseAPI
+import Storage
 import sys
 
 IP_set = set()
 
-def genIPrep(IP_set):
-    file = open('IP.txt', 'a')
-    for ip in IP_set:
-        response = AbuseAPI.API_request(ip)
-        response_row = response.split(',')
-
-        print(response_row[0])
-        print(response_row[4])
-
-        file.write(response)
-
-    file.close()
-
-
 def main():
 
     try:
-        file_pointer = open(sys.argv[1], 'r+') #download recent stuff and change this var
+        file_pointer = open(sys.argv[1], 'r+')
         filedata = file_pointer.readlines()
         for line in filedata:
             split_values = line.split('","')
             if split_values[25][1].isdigit():
                 IP_set.add(split_values[25])
+                Storage.UpdateDB(split_values[6], split_values[5], split_values[25])
 
         print(IP_set)
-        genIPrep(IP_set)
+        #AbuseAPI.genIPrep(IP_set)
     except FileNotFoundError:
         print(f"Error: The file '{sys.argv[1]}' was not found.")
     except IOError as e:
